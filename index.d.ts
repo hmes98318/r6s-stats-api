6e7b83c0718e2068fa0543ec1c33df37a83f5af0
hmes98318 <s0925677069@gmail.com>
2022-10-22 01:14:38 +0800

    Update definition file

--- index.d.ts
+++ index.d.ts
@@ -3,16 +3,15 @@
 import { StatsGeneral } from './types/general';
 import { StatsCasual } from './types/casual';
 import { StatsRank } from './types/rank';
-import { StatsUnrank } from './types/unrank';
 import { StatsDeathmatch } from './types/deathmatch';
 import { StatsOperator } from './types/operator';
 
 
 /**
  * Fetch General stats.
- * @param {string} platform: "pc" | "xbox" | "psn".
+ * @param {string} platform: `pc` | `xbox` | `psn`.
  * @param {string} name: Player name.
- * @returns Stats | "FORMAT_ERROR" | "PLATFORM_ERROR" | "NOT_FOUND" | "TIME_OUT"
+ * @returns Stats | `FORMAT_ERROR` | `PLATFORM_ERROR` | `NOT_FOUND` | `TIME_OUT`
  * @typedef "FORMAT_ERROR": Input format error.
  * @typedef "PLATFORM_ERROR": Input platform error.
  * @typedef "NOT_FOUND": No stats found or any errors.
@@ -22,9 +21,9 @@ async function general(platform: string, name: string): Promise<StatsGeneral | "
 
 /**
  * Fetch Casual stats.
- * @param {string} platform: "pc" | "xbox" | "psn".
+ * @param {string} platform: `pc` | `xbox` | `psn`.
  * @param {string} name: Player name.
- * @returns Stats | "FORMAT_ERROR" | "PLATFORM_ERROR" | "NOT_FOUND" | "TIME_OUT"
+ * @returns Stats | `FORMAT_ERROR` | `PLATFORM_ERROR` | `NOT_FOUND` | `TIME_OUT`
  * @typedef "FORMAT_ERROR": Input format error.
  * @typedef "PLATFORM_ERROR": Input platform error.
  * @typedef "NOT_FOUND": No stats found or any errors.
@@ -34,9 +33,9 @@ declare async function casual(platform: string, name: string): Promise<StatsCasu
 
 /**
  * Fetch Rank stats.
- * @param {string} platform: "pc" | "xbox" | "psn".
+ * @param {string} platform: `pc` | `xbox` | `psn`.
  * @param {string} name: Player name.
- * @returns Stats | "FORMAT_ERROR" | "PLATFORM_ERROR" | "NOT_FOUND" | "TIME_OUT"
+ * @returns Stats | `FORMAT_ERROR` | `PLATFORM_ERROR` | `NOT_FOUND` | `TIME_OUT`
  * @typedef "FORMAT_ERROR": Input format error.
  * @typedef "PLATFORM_ERROR": Input platform error.
  * @typedef "NOT_FOUND": No stats found or any errors.
@@ -46,9 +45,9 @@ declare async function rank(platform: string, name: string): Promise<StatsRank |
 
 /**
  * Fetch Deathmatch stats.
- * @param {string} platform: "pc" | "xbox" | "psn".
+ * @param {string} platform: `pc` | `xbox` | `psn`.
  * @param {string} name: Player name.
- * @returns Stats | "FORMAT_ERROR" | "PLATFORM_ERROR" | "NOT_FOUND" | "TIME_OUT"
+ * @returns Stats | `FORMAT_ERROR` | `PLATFORM_ERROR` | `NOT_FOUND` | `TIME_OUT`
  * @typedef "FORMAT_ERROR": Input format error.
  * @typedef "PLATFORM_ERROR": Input platform error.
  * @typedef "NOT_FOUND": No stats found or any errors.
@@ -58,13 +57,14 @@ declare async function deathmatch(platform: string, name: string): Promise<Stats
 
 /**
  * Fetch Operators stats.
- * @param {string} platform: "pc" | "xbox" | "psn".
+ * @param {string} platform: `pc` | `xbox` | `psn`.
  * @param {string} name: Player name.
- * @returns Stats | "FORMAT_ERROR" | "PLATFORM_ERROR" | "NOT_FOUND" | "TIME_OUT"
+ * @returns Stats | `FORMAT_ERROR` | `PLATFORM_ERROR` | `NOT_FOUND` | `TIME_OUT` | `OPERATOR_ERROR`
  * @typedef "FORMAT_ERROR": Input format error.
  * @typedef "PLATFORM_ERROR": Input platform error.
  * @typedef "NOT_FOUND": No stats found or any errors.
  * @typedef "TIME_OUT": Fetch stats request time out.
+ * @typedef "OPERATOR_ERROR": Wrong operator name.
  */
 declare async function operator(platform: string, name: string, operator: string): Promise<StatsOperator | "ERROR">
 
