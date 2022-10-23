b6b9641debfd5e8ad2ad6ef6f3100640fb2b356a
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-10-24 02:16:11 +0800

    Use same format

--- src/lib/fetchDeathmatch.js
+++ src/lib/fetchDeathmatch.js
@@ -2,44 +2,46 @@ const cheerio = require('cheerio');
 const { exec } = require('child_process');
 const filterArray = require('./modules/filterarray');
 
+
 module.exports = function (url) {
-  let result = [];
-  return new Promise(function (resolve, reject) {
-    exec(`curl --max-time 5 --url ${url}`, (error, stdout, stderr) => {
-      if (!stdout) {
-        result[0] = 'timeout';
-        resolve(result);
-      } else {
-        let rank = [];
+    let result = [];
+    return new Promise(function (resolve, reject) {
+        exec(`curl --max-time 5 --url ${url}`, (error, stdout, stderr) => {
+            if (!stdout) {
+                result[0] = 'timeout';
+                resolve(result);
+            }
 
-        let $ = cheerio.load(stdout);
+            else {
+                let rank = [];
 
-        $('#profile .r6-season__stats').each(function (i, elem) {
-          rank.push(filterArray($(this).text().split('\n')));
+                let $ = cheerio.load(stdout);
+
+                $('#profile .r6-season__stats').each(function (i, elem) {
+                    rank.push(filterArray($(this).text().split('\n')));
+                });
+
+                let imgurl = $('img').map(function () {
+                    return $(this).attr('src');
+                }); //console.log(imgurl.toArray());
+                let header = imgurl.toArray()[0];
+
+                result.push(header);
+
+                if (header.indexOf('avatars') === -1 && header.indexOf('xbox') === -1) {
+                    result[0] = 'error';
+                    resolve(result);
+                }
+
+                result.push(rank[2]);
+
+                //console.log(rank);
+                //console.log(result);
+
+                if (error !== null) reject(error);
+
+                resolve(result);
+            }
         });
-
-        let imgurl = $('img').map(function () {
-          return $(this).attr('src');
-        }); //console.log(imgurl.toArray());
-        let header = imgurl.toArray()[0];
-
-        result.push(header);
-
-        if (header.indexOf('avatars') === -1 && header.indexOf('xbox') === -1) {
-          result[0] = 'error';
-          resolve(result);
-        }
-
-        result.push(rank[2]);
-
-        //console.log(rank);
-        //console.log(result);
-
-        if (error !== null) {
-          reject(error);
-        }
-        resolve(result);
-      }
     });
-  });
 };
--- src/lib/fetchOperator.js
+++ src/lib/fetchOperator.js
@@ -2,57 +2,62 @@ const cheerio = require('cheerio');
 const { exec } = require('child_process');
 const filterArray = require('./modules/filterarray');
 
+
 module.exports = function (url, operator) {
-  let result = [];
-  return new Promise(function (resolve, reject) {
-    exec(`curl --max-time 5 --url ${url}`, (error, stdout, stderr) => {
-      if (!stdout) {
-        result[0] = 'timeout';
-        resolve(result);
-      } else {
-        let profile = [];
+    let result = [];
+    return new Promise(function (resolve, reject) {
+        exec(`curl --max-time 5 --url ${url}`, (error, stdout, stderr) => {
+            if (!stdout) {
+                result[0] = 'timeout';
+                resolve(result);
+            }
 
-        let $ = cheerio.load(stdout);
+            else {
+                let profile = [];
 
-        $('#profile .trn-table__row').each(function (i, elem) {
-          profile.push(filterArray($(this).text().split('\n')));
+                let $ = cheerio.load(stdout);
+
+                $('#profile .trn-table__row').each(function (i, elem) {
+                    profile.push(filterArray($(this).text().split('\n')));
+                });
+
+                let imgurl = $('img').map(function () {
+                    return $(this).attr('src');
+                }); //console.log(imgurl.toArray());
+                let header = imgurl.toArray()[0];
+
+                result.push(header);
+
+                if (header.indexOf('avatars') === -1 && header.indexOf('xbox') === -1) {
+                    result[0] = 'error';
+                    resolve(result);
+                }
+
+                let operator_img = `https://trackercdn.com/cdn/r6.tracker.network/operators/badges/${operator.toLowerCase()}.png`;
+                result.push(operator_img);
+
+                if (operator == 'NAKK') operator = 'NØKK';
+                if (operator == 'JAGER') operator = 'JÄGER';
+                if (operator == 'CAPITAO') operator = 'CAPITÃO';
+
+                result.push(profile[0]);
+
+                for (var i = 0; i < profile.length; i++) {
+                    if (profile[i].indexOf(operator) !== -1) result.push(profile[i]);
+                }
+
+                //console.log(profile);
+                //console.log(result);
+
+                if (error !== null) reject(error);
+
+                if (result.length < 4) {
+                    result[0] = 'operator_error';
+                    resolve(result);
+                }
+
+                resolve(result);
+            }
         });
-
-        let imgurl = $('img').map(function () {
-          return $(this).attr('src');
-        }); //console.log(imgurl.toArray());
-        let header = imgurl.toArray()[0];
-
-        result.push(header);
-
-        if (header.indexOf('avatars') === -1 && header.indexOf('xbox') === -1) {
-          result[0] = 'error';
-          resolve(result);
-        }
-
-        let operator_img = `https://trackercdn.com/cdn/r6.tracker.network/operators/badges/${operator.toLowerCase()}.png`;
-        result.push(operator_img);
-
-        if (operator == 'NAKK') operator = 'NØKK';
-        if (operator == 'JAGER') operator = 'JÄGER';
-        if (operator == 'CAPITAO') operator = 'CAPITÃO';
-
-        result.push(profile[0]);
-
-        for (var i = 0; i < profile.length; i++) {
-          if (profile[i].indexOf(operator) !== -1) result.push(profile[i]);
-        }
-
-        //console.log(profile);
-        //console.log(result);
-
-        if (error !== null) reject(error);
-
-        if (result.length < 4) result[0] = 'operator_error';
-        resolve(result);
-
-        resolve(result);
-      }
     });
-  });
 };
