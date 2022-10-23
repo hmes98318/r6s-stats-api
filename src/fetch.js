bfeeb9f109f60e0a172d7554babd56de8079ccb6
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-10-24 01:59:51 +0800

    Nothing changes

--- src/fetch.js
+++
@@ -1,6 +0,0 @@
-exports.trackGeneral = require('./lib/fetchGeneral');
-exports.trackCasual = require('./lib/fetchCasual');
-exports.trackRank = require('./lib/fetchRank');
-exports.trackUnrank = require('./lib/fetchUnrank');
-exports.trackDeathmatch = require('./lib/fetchDeathmatch');
-exports.trackOperator = require('./lib/fetchOperator');
