13629dc91cd0fb092f328e747db83e579c508c4c
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-10-24 19:47:08 +0800

    Change to use axios to fetch data

--- src/fetch/fetch-general.js
+++ src/fetch/fetch-general.js
@@ -1,53 +1,44 @@
 const cheerio = require('cheerio');
-const { exec } = require('child_process');
+const exec = require('./modules/exec-fetch');
 const filterArray = require('./modules/filterarray');
 
-
-module.exports = function (url) {
+module.exports = async function (url) {
     let result = [];
-    return new Promise(function (resolve, reject) {
-        exec(`curl --max-time 5 --url ${url}`, (error, stdout, stderr) => {
-            if (!stdout) {
-                result[0] = 'timeout';
-                resolve(result);
-            }
+    let profile = [];
 
-            else {
-                let profile = [];
-                //let result = [];
+    const data = await exec(url);
 
-                let $ = cheerio.load(stdout);
+    if (!data) {
+        result[0] = 'timeout';
+        return result;
+    }
 
-                $('#profile .trn-card').each(function (i, elem) {
-                    profile.push(filterArray($(this).text().split('\n')));
-                });
+    const $ = cheerio.load(data);
 
-                let imgurl = $('img').map(function () {
-                    return $(this).attr('src');
-                }); //console.log(imgurl.toArray());
-                let header = imgurl.toArray()[0];
-
-                result.push(header);
-
-                if (header.indexOf('avatars') === -1 && header.indexOf('xbox') === -1) {
-                    result[0] = 'error';
-                    resolve(result);
-                }
-
-                for (var i = 0; i < profile.length; i++) {
-                    if (profile[i].indexOf('Level') !== -1) result.push(profile[i]);
-                }
-                for (var i = 0; i < profile.length; i++) {
-                    if (profile[i].indexOf('General') !== -1) result.push(profile[i]);
-                }
-
-                //console.log(profile);
-                //console.log(result);
-
-                if (error !== null) reject(error);
-
-                resolve(result);
-            }
-        });
+    $('#profile .trn-card').each(function (i, elem) {
+        profile.push(filterArray($(this).text().split('\n')));
     });
+
+    let imgurl = $('img').map(function () {
+        return $(this).attr('src');
+    });
+    let header = imgurl.toArray()[0];
+    result.push(header);
+
+    if (header.indexOf('avatars') === -1 && header.indexOf('xbox') === -1) {
+        result[0] = 'error';
+        return result;
+    }
+
+    for (var i = 0; i < profile.length; i++) {
+        if (profile[i].indexOf('Level') !== -1) result.push(profile[i]);
+    }
+    for (var i = 0; i < profile.length; i++) {
+        if (profile[i].indexOf('General') !== -1) result.push(profile[i]);
+    }
+
+    //console.log(profile);
+    //console.log(result);
+
+    return result;
 };
--- src/fetch/fetch-unrank.js
+++ src/fetch/fetch-unrank.js
@@ -1,49 +1,42 @@
 const cheerio = require('cheerio');
-const { exec } = require('child_process');
+const exec = require('./modules/exec-fetch');
 const filterArray = require('./modules/filterarray');
 
 
-module.exports = function (url) {
+module.exports = async function (url) {
     let result = [];
-    return new Promise(function (resolve, reject) {
-        exec(`curl --max-time 5 --url ${url}`, (error, stdout, stderr) => {
-            if (!stdout) {
-                result[0] = 'timeout';
-                resolve(result);
-            }
+    let profile = [];
 
-            else {
-                let profile = [];
+    const data = await exec(url);
 
-                let $ = cheerio.load(stdout);
+    if (!data) {
+        result[0] = 'timeout';
+        return result;
+    }
 
-                $('#profile .trn-card').each(function (i, elem) {
-                    profile.push(filterArray($(this).text().split('\n')));
-                });
+    const $ = cheerio.load(stdout);
 
-                let imgurl = $('img').map(function () {
-                    return $(this).attr('src');
-                }); //console.log(imgurl.toArray());
-                let header = imgurl.toArray()[0];
-
-                result.push(header);
-
-                if (header.indexOf('avatars') === -1 && header.indexOf('xbox') === -1) {
-                    result[0] = 'error';
-                    resolve(result);
-                }
-
-                for (var i = 0; i < profile.length; i++) {
-                    if (profile[i].indexOf('UnRanked') !== -1) result.push(profile[i]);
-                }
-
-                //console.log(profile);
-                //console.log(result);
-
-                if (error !== null) reject(error);
-
-                resolve(result);
-            }
-        });
+    $('#profile .trn-card').each(function (i, elem) {
+        profile.push(filterArray($(this).text().split('\n')));
     });
+
+    let imgurl = $('img').map(function () {
+        return $(this).attr('src');
+    });
+    let header = imgurl.toArray()[0];
+    result.push(header);
+
+    if (header.indexOf('avatars') === -1 && header.indexOf('xbox') === -1) {
+        result[0] = 'error';
+        return result;
+    }
+
+    for (var i = 0; i < profile.length; i++) {
+        if (profile[i].indexOf('UnRanked') !== -1) result.push(profile[i]);
+    }
+
+    //console.log(profile);
+    //console.log(result);
+
+    return result;
 };
