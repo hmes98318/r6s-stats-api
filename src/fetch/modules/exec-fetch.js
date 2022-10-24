87d768892affd23f800333a81bd7c82325d3c01a
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-10-24 19:48:34 +0800

    axios module

---
+++ src/fetch/modules/exec-fetch.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const agent = require('./random-agent');
+
+module.exports = async function (url) {
+
+    const { data } = await axios({
+        method: 'GET',
+        url: url,
+        headers: {
+            'Accept': 'text/html',
+            'accept-encoding': 'deflate',
+            'accept-language': 'en-US,en;q=0.8,et;q=0.6',
+            'cache-control': 'max-age=0'
+        },
+        timeout: 5000,
+    })
+        .catch((error) => { return error; });
+
+    return data;
+}
+
+
